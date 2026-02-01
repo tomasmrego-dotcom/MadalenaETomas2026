b7521ff3b9169fca82900490d0f65cd71c955e70
TomasBlocklabs <tomas@blocklabs.io>
2026-02-01 12:21:35 +0000

    V0.13

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/MadalenaETomas2026',
+  basePath: '/Wedding-Website-v2',
   images: {
     unoptimized: true,
   },
